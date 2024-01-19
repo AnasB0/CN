6adef74b4bef7dc33f993606965a20cd17568f9e
Mustafa Anas Basheer <134994753+AnasB0@users.noreply.github.com>
2024-01-19 22:30:28 +0530

    3a.c

--- 3a.c
+++ 3a.c
@@ -4,7 +4,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
-#include<sys/socket.h>
+#include<sys/socket.h> 
 #include<unistd.h>
 #include<netinet/in.h>
 main(int argc,char* argv[])
